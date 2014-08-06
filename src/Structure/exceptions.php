bf49b58e201db771d6bdd075976b8a97f2efc21b
Adric Schreuders <adric@schreuders.org>
2014-08-06 09:33:36 +0800

    Add UK spelling for UnauthorisedException

--- src/Structure/exceptions.php
+++ src/Structure/exceptions.php
@@ -8,5 +8,6 @@ class CacheException extends DataAccessException {}
 // User Safe Exceptions are exceptions where the message is safe to display to the user
 class UserSafeException extends RuntimeException {}
 class UnauthorizedException extends UserSafeException {}
+class UnauthorisedException extends UnauthorizedException {}
 class MissingAssetException extends UserSafeException {}
 
